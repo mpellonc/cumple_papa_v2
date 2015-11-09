b5d67a555a214dad1ee69aa715767ca9e9dc0bcd
mpellonc <marina.consunji@gmail.com>
2015-11-08 20:58:33 -0500

    add message and pictures

--- app.rb
+++ app.rb
@@ -1,6 +1,37 @@
 require 'sinatra'
 
 get '/' do
-  "cumple papa!"
-  erb :show
+  erb :index
+end
+
+get '/uno' do
+  erb :uno
+end
+
+get '/dos' do
+  erb :dos
+end
+
+get '/tres' do
+  erb :tres
+end
+
+get '/cuatro' do
+  erb :cuatro
+end
+
+get '/cinco' do
+  erb :cinco
+end
+
+get '/seis' do
+  erb :seis
+end
+
+get '/siete' do
+  erb :siete
+end
+
+get '/ocho' do
+  erb :ocho
 end
